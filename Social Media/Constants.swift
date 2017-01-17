753265d59e53cfac55a438801010b8c48b769cb7
apple <apple@apples-iMac.local>
2017-01-17 10:26:07 -0500

    Sign in custom classes completed

---
+++ Social Media/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Social Media
+//
+//  Created by apple on 1/17/17.
+//  Copyright © 2017 Nil. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
+
---
+++ Social Media/FancyField.swift
@@ -0,0 +1,33 @@
+//
+//  FancyField.swift
+//  Social Media
+//
+//  Created by apple on 1/17/17.
+//  Copyright © 2017 Nil. All rights reserved.
+//
+
+import UIKit
+
+class FancyField: UITextField {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.borderColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.2).cgColor
+        layer.borderWidth = 1.0
+        layer.cornerRadius = 5.0
+        
+        
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: 10, dy: 5)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: 10, dy: 5)
+    }
+    
+    
+    
+}
